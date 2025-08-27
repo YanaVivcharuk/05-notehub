6c18968710a2a3fc1af8f183b0df931066d2c9ab
YanaVivcharuk <yanavivcharuk@icloud.com>
2025-08-28 00:40:12 +0200

    edit

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./components/App/App.module.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import App from "./components/App/App";
+import App from "./components/App/App.tsx";
 import "modern-normalize/modern-normalize.css";
 
 const queryClient = new QueryClient();
